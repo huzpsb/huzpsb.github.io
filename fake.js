1c4831967213ca8dbd8d10d61f34bf82697be9b1
John Smith <41772578+huzpsb@users.noreply.github.com>
2022-12-04 20:25:31 +0800

    Add files via upload

---
+++ fake.js
@@ -0,0 +1,8 @@
+var messages = [
+    "§6[§bMythicMobs§6] §3感谢使用 §e光の神话 §3破解版",
+    "§6[§bMythicMobs§6] §cCracked by 耀星",
+    '§6[§bMythicMobs§6] §3如有问题请加QQ群: 1095757683.',
+]
+messages.forEach(function (message) {
+    Bukkit.broadcastMessage(message)
+})
