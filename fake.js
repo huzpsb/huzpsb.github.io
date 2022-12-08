5bcf23d3e9774543577d054775561b60699b6ce4
John Smith <41772578+huzpsb@users.noreply.github.com>
2022-12-08 11:26:08 +0800

    Update fake.js

--- fake.js
+++ fake.js
@@ -1,9 +1,1 @@
-var Bukkit = Java.type('org.bukkit.Bukkit')
-var messages = [
-    "§6[§bMythicMobs§6] §3感谢使用 §e光の神话 §3破解版",
-    "§6[§bMythicMobs§6] §cCracked by 耀星",
-    '§6[§bMythicMobs§6] §3如有问题请加QQ群: 1095757683.',
-]
-messages.forEach(function (message) {
-    Bukkit.broadcastMessage(message)
-})
+
